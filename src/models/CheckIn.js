c3c6da7129f29f4814ad24a0540b1ed147766bb0
ntnt <ntnt@192.168.101.102>
2025-05-13 18:37:31 +0700

    Initial project

--- src/models/CheckIn.js
+++ src/models/CheckIn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const checkInSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   checkInTime: { type: Date, default: Date.now },
-  rewardPointsEarned: { type: Number, default: 10 },
+  rewardPointsEarned: { type: Number, default: 1 },
 });
 
 module.exports = mongoose.model('CheckIn', checkInSchema);
